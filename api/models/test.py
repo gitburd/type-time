cab2909c1406d54abe0d8165cff0e5813d7fcb7b
Dena Burd <denaburd@gmail.com>
2021-07-26 21:51:09 -0700

    Accuracy is an Integer

--- api/models/test.py
+++ api/models/test.py
@@ -4,7 +4,7 @@ from app import db
 class Test(db.Model):
     id = db.Column(db.Integer, primary_key=True, autoincrement=True)
     category = db.Column(db.String)
-    accuracy = db.Column(db.String)
+    accuracy = db.Column(db.Integer)
     timer = db.Column(db.Integer)
     totalWordCount = db.Column(db.Integer)
     wordsPerMin = db.Column(db.Integer)
